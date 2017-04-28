0dcf6bbb0eb270b0fed5639f6ce6890af7aafe42
Tu Ngo <tungo8810@gmail.com>
2017-04-28 14:18:52 -0700

    Create Function.prototype.curry

--- arguments-exercises/curried_sum.js
+++ arguments-exercises/curried_sum.js
@@ -15,5 +15,55 @@ function curriedSum(numArgs) {
   return _curriedSum;
 }
 
-const sum = curriedSum(4);
-console.log(sum(5)(30)(20)(1));
+// const sum = curriedSum(4);
+// console.log(sum(5)(30)(20)(1));
+
+Function.prototype.curry = function (numArgs) {
+  let that = this;
+  const args = [];
+
+  function _curry(arg) {
+    args.push(arg);
+
+    if (args.length === numArgs) {
+      return that.apply(null, args);
+    } else {
+      return _curry;
+    }
+  }
+
+  return _curry;
+};
+
+let sum = function (num1, num2, num3) {
+  return num1 + num2 + num3;
+};
+
+let s = sum.curry(3);
+console.log(s);
+
+let s1 = s(1);
+console.log(s1);
+
+let s2 = s1(2);
+console.log(s2);
+
+let s3 = s2(3);
+console.log(s3);
+
+
+
+// class Cat {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//
+//   says(sound, person) {
+//     console.log(`${this.name} says ${sound} to ${person}!`);
+//     return true;
+//   }
+// }
+//
+// let cat = new Cat('kitty');
+// let cc = cat.says.curry(2);
+// console.log(cc('meow')('Alex'));
